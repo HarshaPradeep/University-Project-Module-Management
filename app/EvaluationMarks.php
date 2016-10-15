4c2943e8604f8c96802f6ffc83e9a19f5b9befe6
HarshaPradeep <HarshDell@192.168.1.3>
2016-10-15 23:00:23 +0530

    Added / Modified by Harsha Pradeep

---
+++ app/EvaluationMarks.php
@@ -0,0 +1,28 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EvaluationMarks extends Model {
+
+    protected $table = 'evaluation_mark';
+    
+    protected $fillable = [
+            
+            'stuid',
+            'stuemail',
+            'stugrpid',
+            'proposalpresent',
+            'proposalreport',
+            'srsreport',
+            'protopresent',
+            'midpresent',
+            'midreport',
+            'finalprsent',
+            'finalreport',
+            'viva',
+            'researchbook',
+            'researchpaper',
+            'website',
+            'status'
+        ];
+}
