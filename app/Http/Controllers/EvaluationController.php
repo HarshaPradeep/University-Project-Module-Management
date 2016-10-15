b569494f2d250957f634215529df8aefdc29a16c
HarshaPradeep <HarshDell@192.168.1.3>
2016-10-15 23:01:19 +0530

    Added / Modified by Harsha Pradeep

--- app/Http/Controllers/EvaluationController.php
+++ app/Http/Controllers/EvaluationController.php
@@ -1,10 +1,9 @@
 <?php namespace App\Http\Controllers;
 
-use App\Http\Requests;
-use App\Http\Controllers\Controller;
-
-use Illuminate\Http\Request;
-use \DB;
+use App\EvaluationMarks;
+use DB;
+use Request;
+use Input;
 class EvaluationController extends Controller {
 
 	/**
@@ -30,26 +29,32 @@ class EvaluationController extends Controller {
 	 */
 	public function create()
 	{
-                /*Get all supervisor name s acording to the supervisor id*/
+                /*Get all supervisor names acording to the supervisor id*/
                 $supervisaornames = DB::select("SELECT name,id 
 		 FROM panelmembers
 		 WHERE id = any (SELECT supervisorId 
 		 FROM projects
 		 WHERE status = 'Approved')");
-
+                
+                /*Get all group ids*/
+                $groupids = DB::select("SELECT groupID 
+		 FROM research_groups");
+ 
                 /*Get all student actual id acording to the auto generated id*/
                 $studentid = DB::select("SELECT id,regId 
 		 FROM students
 		 WHERE id = any (SELECT studentId 
 		 FROM projects
 		 WHERE status = 'Approved')");
-        
+                                
                 /*filtering projects according to the groups*/
 		$students = DB::select("SELECT id,title,studentId,supervisorId 
 		 FROM projects
-		 WHERE status = 'Approved'");
+		 WHERE status = 'Approved' and groupIDforTitle = any (SELECT groupID 
+		 FROM research_groups)");
+                
               
-            return view('evaluation.evaluationform', compact('students', 'supervisaornames', 'studentid'));
+            return view('evaluation.evaluationform', compact('students', 'supervisaornames', 'studentid', 'groupids'));
 	}
 
 	/**
@@ -58,9 +63,52 @@ class EvaluationController extends Controller {
 	 * @return Response
 	 */
 	public function store()
-	{
-		//
-	}
+        {
+            //dd(Request::get('marksformidpresentab1'));
+            EvaluationMarks::create([
+                
+                //Tab 1(LEADER)
+                'stugrpid' => Input::get('selectid'),
+                'stuid' => Request::get('cmntmem0'),
+                'proposalpresent' => Request::get('marksforproposalpresenttab1'),
+                'proposalreport' => Request::get('marksforproposalreporttab1'),
+                'srsreport' => Request::get('marksforsrstab1'),
+                'protopresent' => Request::get('marksforprototab1'),
+                'midpresent' => Request::get('marksformidpresentab1'),
+                'midreport' => Request::get('marksfomidreporttab1'),
+                'finalprsent' => Request::get('marksforfinalpresnttab1'),
+                'finalreport' => Request::get('marksforfinalreporttab1'),
+                'viva' => Request::get('marksforvivatab1'),
+                'researchbook' => Request::get('marksforresearchbooktab1'),
+                'researchpaper' => Request::get('marksforresearchpapertab1'),
+                'website' => Request::get('marksforwebsitetab1'),
+                'status' => Input::get('status')
+                //Tab 1 ends
+               ]);
+            
+            EvaluationMarks::create([
+                
+                //Tab 1(LEADER)
+                'stugrpid' => Input::get('selectid'),
+                'stuid' => Request::get('cmntmem0'),
+//                'proposalpresent' => Request::get('marksforproposalpresenttab1'),
+//                'proposalreport' => Request::get('marksforproposalreporttab1'),
+//                'srsreport' => Request::get('marksforsrstab1'),
+//                'protopresent' => Request::get('marksforprototab1'),
+//                'midpresent' => Request::get('marksformidpresentab1'),
+//                'midreport' => Request::get('marksfomidreporttab1'),
+//                'finalprsent' => Request::get('marksforfinalpresnttab1'),
+//                'finalreport' => Request::get('marksforfinalreporttab1'),
+//                'viva' => Request::get('marksforvivatab1'),
+//                'researchbook' => Request::get('marksforresearchbooktab1'),
+//                'researchpaper' => Request::get('marksforresearchpapertab1'),
+//                'website' => Request::get('marksforwebsitetab1'),
+//                'status' => Input::get('status')
+                //Tab 1 ends
+               ]);
+
+            return redirect('evaluationform');
+        }
 
 	/**
 	 * Display the specified resource.
