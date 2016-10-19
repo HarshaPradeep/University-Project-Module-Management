0bedef580913c0cdc333371c114fb02bfdbe8299
HarshaPradeep <HarshDell@192.168.1.3>
2016-10-20 00:51:49 +0530

    Modified by Harsha Pradeep

--- app/Http/Controllers/supevaluationController.php
+++ app/Http/Controllers/supevaluationController.php
@@ -1,9 +1,9 @@
 <?php namespace App\Http\Controllers;
 
-use App\Http\Requests;
 use App\Http\Controllers\Controller;
-
-use Illuminate\Http\Request;
+use DB;
+use Request;
+use Input;
 
 class supevaluationController extends Controller {
 
@@ -28,9 +28,66 @@ class supevaluationController extends Controller {
 	 *
 	 * @return Response
 	 */
-	public function create()
+        
+        /*Proposal*/
+	public function createproppresen()
 	{
-            return view('supevaluation.propevaluation');
+            /*Get all supervisor names acording to the supervisor id*/
+                $supervisaornames = DB::select("SELECT name,id 
+		 FROM panelmembers
+		 WHERE id = any (SELECT supervisorId 
+		 FROM projects
+		 WHERE status = 'Approved')");
+                
+                /*Get all group ids*/
+                $groupids = DB::select("SELECT groupID 
+		 FROM research_groups");
+ 
+                /*Get all student actual id acording to the auto generated id*/
+                $studentid = DB::select("SELECT id,regId 
+		 FROM students
+		 WHERE id = any (SELECT studentId 
+		 FROM projects
+		 WHERE status = 'Approved')");
+                                
+                /*filtering projects according to the groups*/
+		$students = DB::select("SELECT id,title,studentId,supervisorId 
+		 FROM projects
+		 WHERE status = 'Approved' and groupIDforTitle = any (SELECT groupID 
+		 FROM research_groups)");
+                
+                
+            return view('supevaluation.propevaluation', compact('students', 'supervisaornames', 'studentid', 'groupids'));
+	}
+        
+        public function createproreport()
+	{
+            /*Get all supervisor names acording to the supervisor id*/
+                $supervisaornames = DB::select("SELECT name,id 
+		 FROM panelmembers
+		 WHERE id = any (SELECT supervisorId 
+		 FROM projects
+		 WHERE status = 'Approved')");
+                
+                /*Get all group ids*/
+                $groupids = DB::select("SELECT groupID 
+		 FROM research_groups");
+ 
+                /*Get all student actual id acording to the auto generated id*/
+                $studentid = DB::select("SELECT id,regId 
+		 FROM students
+		 WHERE id = any (SELECT studentId 
+		 FROM projects
+		 WHERE status = 'Approved')");
+                                
+                /*filtering projects according to the groups*/
+		$students = DB::select("SELECT id,title,studentId,supervisorId 
+		 FROM projects
+		 WHERE status = 'Approved' and groupIDforTitle = any (SELECT groupID 
+		 FROM research_groups)");
+                
+              
+            return view('supevaluation.propreportevaluation', compact('students', 'supervisaornames', 'studentid', 'groupids'));
 	}
         
         public function srscreate()
@@ -47,6 +104,59 @@ class supevaluationController extends Controller {
         {
             return view('supevaluation.midevaluation');
         }
+        
+        function searchforpropStudents() {
+
+        $searchId = Input::get('sid');
+        
+        /*get project title according to the student's id*/
+	//$protitle = \App\Evaluation::where('studentId', $searchId)->pluck('title');
+        $protitle = DB::table('projects')
+                    ->where('groupIDforTitle', $searchId)
+                    ->Where('status', 'Approved')
+                    ->pluck('title');
+        
+        /*get project id according to the student's id*/
+        //$proid = \App\Evaluation::where('studentId', $searchId)->pluck('id');
+        $proid = DB::table('projects')
+                    ->where('title', $protitle)
+                    ->Where('status', 'Approved')
+                    ->pluck('id');
+        
+        /*mail adds. of members in the group*/
+        $groupmails = DB::table('research_groups')
+                        ->where('groupID', $searchId)
+                        ->pluck('mails');
+        
+        $explodedemails = explode("/",$groupmails);
+        
+//        $idss= DB::table('students')
+//                        ->where('email', $explodedemails)
+//                        ->pluck('regId');
+
+        $sendids = '';
+        $sendnames = '';
+        foreach ($explodedemails as $mail)
+        {
+           $id = (DB::table('students')
+                   ->where('email', $mail)
+                   ->pluck('regId'));
+           
+           $sendids .= $id.'/'; 
+           
+           /*get student name according to the student's id*/
+            $stuname = (DB::table('students')
+                    ->where('email', $mail)
+                    ->pluck('name'));
+            $sendnames .= $stuname.'/'; 
+        }
+        
+        $explodedsendids = explode("/", rtrim($sendids, "/"));
+        $explodedsendnames = explode("/", rtrim($sendnames, "/"));
+
+        $data = array("title" => $protitle, "pid" => $proid, "sname" => $explodedsendnames, "noofstu" => sizeof($explodedemails), "ids" => $explodedsendids, "ledrid" => $explodedsendids[0]);
+        return json_encode($data);
+    }
         
         /**
 	 * Store a newly created resource in storage.
