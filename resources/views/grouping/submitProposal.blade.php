51c7b5d9825943b898737c0efd4d4201ef11a3cb
HarshaPradeep <HarshDell@192.168.1.2>
2016-11-01 00:34:18 +0530

    Modified by Harsha Pradeep

--- resources/views/grouping/submitProposal.blade.php
+++ resources/views/grouping/submitProposal.blade.php
@@ -46,6 +46,7 @@
                 <li>{{$error}}</li>
             @endforeach
         </div>
+    @endif
 
     @if(Session::has('flash_message'))
         <div class="alert alert-success"><span class="glyphicon glyphicon-ok"></span><em> {!! session('flash_message') !!}</em></div>
