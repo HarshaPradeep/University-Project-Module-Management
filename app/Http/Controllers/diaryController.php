75e073e3020397585662043071bcf5a45fba9ece
DiluniAmarasekara <dilunia6@gmail.com>
2016-11-11 20:28:07 +0530

    Update diaryController.php

--- app/Http/Controllers/diaryController.php
+++ app/Http/Controllers/diaryController.php
@@ -1,18 +1,26 @@
 <?php namespace App\Http\Controllers;
 
+use App\defect;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-
+use App\task;
 use Illuminate\Http\Request as Request2;
+use Illuminate\Support\Facades\Input;
 use Request;
+use App\intreport;
+use App\Student;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\View;
+use Cartalyst\Sentinel\Laravel\Facades\Sentinel;
+
 
 class diaryController extends Controller {
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
+
     
         public function __construct() 
         {
@@ -24,93 +32,101 @@ class diaryController extends Controller {
 		//
 	}
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
+	public function getdata(){
+
+
+	    $id = Student::where('email',Sentinel::getUser()->email)->get()[0]->regId;
+        $groupid = defect::where('studentId' , $id )->get()[0]->groupId;
+
+	    $start =  defect::where(['state' => 'Start', 'groupId' => $groupid])->count();
+        $finish =  defect::where(['state' => 'Finish', 'groupId' => $groupid])->count();
+        $not_start =  defect::where(['state' => 'Not Start', 'groupId' => $groupid])->count();
+
+        return ["start" => $start, 'not_start' => $not_start , 'finish' => $finish];
+
+    }
+
+
 	public function create()
 	{
 		return view('researchDiary/diaryhome');
 	}
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
+	public function createchrt()
+	{
+		return view('researchDiary/analysis');
+	}
+	
+    public function createtas($id)
+    {
+        $task =  task::where('id',$id)->get();
+
+        return view('researchDiary/tupdate',compact('task'));
+    }
+
         public function taskopen()
         {
-            $Alltasks = \App\schedule::all();
+            $currentUser = Sentinel::getUser()["email"];
+            $userid = Student::where('email',$currentUser)->pluck("regid");
+            $groupid = Student::where('email',$currentUser)->pluck("grouped");
+
+            $Alltasks = task::where('groupId',$groupid)->get();
             return view('researchDiary/tasks', compact('Alltasks'));
         }
         
 	public function storeTasks(Request2 $reques)
         {
-
-            \App\schedule::create([
+			$currentUser = Sentinel::getUser()["email"];
+            $userid = Student::where('email',$currentUser)->pluck("regid");
+            $groupid = Student::where('email',$currentUser)->pluck("grouped");
+			
+            \App\task::create([
+				'studentId' => $userid,
+                'groupId' => $groupid,
                 'task' => Request::get('entertask'),
                 'description' => Request::get('desc'),
                 'plantofinish' => Request::get('plantof'),
+                'state' => Request::get('optionsRadios'),
                 'sdate' => Request::get('start'),
-               'edate' => Request::get('end'),
+                'edate' => Request::get('end'),
                 'hours' => Request::get('spenthours')
 
                 ]);
 
-            return redirect('tasks');
+            return redirect('tasks')->with('message_success', 'Task is added!');
         }
         
         public function destroy($id)
         {
-            //$idi = Crypt::decrypt($id);
-            //dd($id);
-            $model = \App\schedule::find($id);
-            //dd($model);
+
+            $model = \App\task::find($id);
+
             $model->delete();
 
-            return redirect('tasks')->with('message_success', 'Task Deleted!');
+            return redirect('tasks')->with('message_success', 'Task is deleted!');
         }
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
+	
+    public function updateTask($id)
     {
-        //
 
-        /**
-         * Update the specified resource in storage.
-         *
-         * @param  int $id
-         * @return Response
-         */
+        $tsk = Input::get('entertask');
+        $des=Input::get('desc');
+        $ptf = Input::get('plantof');
+        $st = Input::get('optionsRadios');
+        $sd=Input::get('start');
+        $ed = Input::get('end');
+        $hr = Input::get('spenthours');
+
+
+        $q = task::where('id', $id)->update(['task' => $tsk,'description'=>$des,'plantofinish'=>$ptf,
+            'state' => $st, 'sdate' => $sd, 'edate' => $ed,
+            'hours' => $hr]);
+
+
+
+        return redirect('tasks')->with('message_success', 'Task is updated!');
+
+
     }
-        public function update($id)
-    {
-        //
-
-
-        /**
-         * Remove the specified resource from storage.
-         *
-         * @param  int  $id
-         * @return Response
-         */
-    }
-
 }
