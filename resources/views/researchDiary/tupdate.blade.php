5a054e472e04e389fcf8a6a0c81e3e5481684163
HarshaPradeep <HarshDell@HarshDell-PC>
2016-11-20 11:51:55 +0530

    Modified by Harsha Pradeep

--- resources/views/researchDiary/tupdate.blade.php
+++ resources/views/researchDiary/tupdate.blade.php
@@ -49,91 +49,71 @@
                 <div class="form-group">
                     <input name="plantof" class="form-control" value="{{ $task[0]->plantofinish }}" required>
                 </div>
-
+                
                 <div class="form-group">
-                    <label>Defect State</label>
-                    <div class="radio" >
+                            <label>Task State</label>
+                            <div class="radio">
+                                @if($task[0]->state == "Start")
 
-                        @if($task[0]->state == "Start")
+                        <div class="form-group">
+                            <label>
+                                <input type="radio" name="optionsRadios" id="optionsRadios1" value="Not Start" >Not Start
+                            </label>
+                        </div>
+                        <div class="form-group">
+                            <label>
+                                <input type="radio" name="optionsRadios" id="optionsRadios2" value="Start" checked>Start
+                            </label>
+                        </div>
+                        <div class="form-group">
+                            <label>
+                                <input type="radio" name="optionsRadios" id="optionsRadios3" value="Finish" >Finish
+                            </label>
+                        </div>
 
-                            <div class="form-group">
-                                <label>
-                                    <input type="radio" name="optionsRadios" id="optionsRadios1" value="Not Start" >Not Start
-                                </label>
+                    @elseif($task[0]->state == "Not Start")
+
+                        <div class="form-group">
+                            <label>
+                                <input type="radio" name="optionsRadios" id="optionsRadios1" value="Not Start" checked>Not Start
+                            </label>
+                        </div>
+                        <div class="form-group">
+                            <label>
+                                <input type="radio" name="optionsRadios" id="optionsRadios2" value="Start">Start
+                            </label>
+                        </div>
+                        <div class="form-group">
+                            <label>
+                                <input type="radio" name="optionsRadios" id="optionsRadios3" value="Finish" >Finish
+                            </label>
+                        </div>
+
+                    @elseif($task[0]->state == "Finish")
+
+                        <div class="form-group">
+                        <label>
+                            <input type="radio" name="optionsRadios" id="optionsRadios1" value="Not Start" >Not Start
+                        </label>
                             </div>
-                            <div class="form-group">
-                                <label>
-                                    <input type="radio" name="optionsRadios" id="optionsRadios2" value="Start" checked>Start
-                                </label>
+                        <div class="form-group">
+                        <label>
+                            <input type="radio" name="optionsRadios" id="optionsRadios2" value="Start" >Start
+                        </label>
                             </div>
-                            <div class="form-group">
-                                <label>
-                                    <input type="radio" name="optionsRadios" id="optionsRadios3" value="Finish" >Finish
-                                </label>
-                            </div>
-
-                        @elseif($task[0]->state == "Not Start")
-
-                            <div class="form-group">
-                                <label>
-                                    <input type="radio" name="optionsRadios" id="optionsRadios1" value="Not Start" checked>Not Start
-                                </label>
-                            </div>
-                            <div class="form-group">
-                                <label>
-                                    <input type="radio" name="optionsRadios" id="optionsRadios2" value="Start">Start
-                                </label>
-                            </div>
-                            <div class="form-group">
-                                <label>
-                                    <input type="radio" name="optionsRadios" id="optionsRadios3" value="Finish" >Finish
-                                </label>
+                        <div class="form-group">
+                        <label>
+                            <input type="radio" name="optionsRadios" id="optionsRadios3" value="Finish" checked>Finish
+                        </label>
                             </div>
 
-                        @elseif($task[0]->state == "Finish")
-
-                            <div class="form-group">
-                                <label>
-                                    <input type="radio" name="optionsRadios" id="optionsRadios1" value="Not Start" >Not Start
-                                </label>
-                            </div>
-                            <div class="form-group">
-                                <label>
-                                    <input type="radio" name="optionsRadios" id="optionsRadios2" value="Start" >Start
-                                </label>
-                            </div>
-                            <div class="form-group">
-                                <label>
-                                    <input type="radio" name="optionsRadios" id="optionsRadios3" value="Finish" checked>Finish
-                                </label>
-                            </div>
-
-                        @endif
-
-                    </div>
-
-                </div>
-
-
-                <div class="form-group">
-                    <input name="start" class="form-control" value="{{ $task[0]->sdate }}" >
-                </div>
-
-                <div class="form-group">
-                    <input name="end" class="form-control" value="{{ $task[0]->edate }}" >
-                </div>
-
-                <div class="form-group">
-                    <input name="spenthours" class="form-control" value="{{ $task[0]->hours }}" >
-                </div>
-
-
+                    @endif
 
                 <div class="summernote"></div>
 
                 <div class="form-group">
                     
-					<div class="col-md-8" style="padding-left:100px;">
+                    <div class="col-md-8" style="padding-left:100px;">
                         <button onclick="updateTask('{{ $task[0]->id }}')" name="addNotice" id="adda"class="btn btn-w-m btn-primary">Update</button>
                         <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
 
@@ -155,6 +135,32 @@
         $(document).ready(function () {
             $('.summernote').ckeditor();
         });
+        
+         $("#optionsRadios1").change(function() {
+                    if(this.checked) {
+                        $("#start_date").children().prop('disabled',true);
+                        $("#end_date").children().prop('disabled',true);
+                        $("#spent_hours").children().prop('disabled',true);
+                    }
+                });
+
+
+                $("#optionsRadios2").change(function() {
+                    if(this.checked) {
+                        $("#start_date").children().prop('disabled',false);
+                        $("#end_date").children().prop('disabled',true);
+                        $("#spent_hours").children().prop('disabled',true);
+                    }
+                });
+
+                $("#optionsRadios3").change(function() {
+                    if(this.checked) {
+                        $("#start_date").children().prop('disabled',false);
+                        $("#end_date").children().prop('disabled',false);
+                        $("#spent_hours").children().prop('disabled',false);
+                    }
+                });
+        
     </script>
 
 @endsection
